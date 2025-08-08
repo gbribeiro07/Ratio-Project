a45c67585e541469a7ba13ecedd53b2566b8c5be
Gabriel <gabrielribeiroalmeida0@gmail.com>
2025-08-08 11:12:06 -0300

    Conexão básica entre o Back-end e o Front-end feita.

---
+++ Frontend/src/Services/Api.js
@@ -0,0 +1,14 @@
+const RATIO_API_URL = import.meta.env.API_URL;
+
+// Função para cadastrar um usuário
+export async function registerUser(nameUser, email, password) {
+  const response = await fetch(`${RATIO_API_URL}/Cadastro`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ nameUser, email, password }),
+  });
+
+  return response.json();
+}
